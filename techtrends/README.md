614c88726166f5a11479aeb92855f4e513bdd6bd
Shrihari Vasudevan <shrihari.vasudevan@gmail.com>
2022-02-06 10:26:25 +0000

    Step 1: Applying best practices for application development

--- techtrends/README.md
+++ techtrends/README.md
@@ -7,4 +7,7 @@ This is a Flask application that lists the latest articles within the cloud-nati
 To run this application there are 2 steps required:
 
 1. Initialize the database by using the `python init_db.py` command. This will create or overwrite the `database.db` file that is used by the web application.
+
 2.  Run the TechTrends application by using the `python app.py` command. The application is running on port `3111` and you can access it by querying the `http://127.0.0.1:3111/` endpoint.
+    * If using a VM with a private network and static IP address, use the static IP address in place of 127.0.0.1 to access it from the host machine's browser. In the current case, as per the Vagrantfile, the IP address is 192.168.56.0 so the endpoint to query would be `http://192.168.56.0:3111/`
+    * To separately check the output logs and error logs, try `python app.py 2> error.txt 1> output.txt`
--- techtrends/app.py
+++ techtrends/app.py
@@ -1,13 +1,16 @@
 import sqlite3
-
+import logging
+import sys
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global num_connections
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    num_connections += 1
     return connection
 
 # Function to get a post using its ID
@@ -36,13 +39,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      app.logger.info('An article with ID %d does not exist' % post_id)
       return render_template('404.html'), 404
     else:
+      app.logger.info('Article "%s" retrieved!' % post[2])
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info('"About TechTrends" page retrieved!')
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -60,11 +66,49 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
+            app.logger.info('New article created!')
 
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
+# Define Healthcheck endpoint
+@app.route('/healthz')
+def healthcheck():
+    
+    response = app.response_class(
+	response = json.dumps({"result" : "OK - healthy"}),
+	status = 200,
+	mimetype = 'application/json')
+
+    return response
+
+
+# Define Metrics endpoint
+@app.route('/metrics')
+def metrics():
+
+    connection = get_db_connection()
+    num_posts = len( connection.execute('SELECT * FROM posts').fetchall() )
+    connection.commit()
+    connection.close()
+
+    response = app.response_class(
+	response = json.dumps({"db_connection_count": num_connections, "post_count": num_posts}),
+	status = 200,
+	mimetype = 'application/json')
+
+    return response	
+
 # start the application on port 3111
 if __name__ == "__main__":
+   num_connections = 1                     # for initial DB creation
+
+   log_format = '%(levelname)s:%(name)s:%(asctime)s, %(message)s\n' 
+   date_format = "%d/%b/%Y, %H:%M:%S"
+   output_handler = logging.StreamHandler(sys.stdout)
+   error_handler = logging.StreamHandler(sys.stderr)
+   logging.basicConfig(level = logging.DEBUG, format = log_format, datefmt = date_format,
+                       handlers = [output_handler, error_handler]) 
+
    app.run(host='0.0.0.0', port='3111')
